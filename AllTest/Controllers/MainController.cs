1a4b4d232f45e38e4beed638a963aeba2c4db63d
单行道 <294736305@qq.com>
2018-02-07 15:13:26 +0800

    测试

---
+++ AllTest/Controllers/MainController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace AllTest.Controllers
+{
+    public class MainController : Controller
+    {
+        public IActionResult AngularJSTest()
+        {
+            return View();
+        }
+
+        public IActionResult AjaxTest(string a)
+        {
+
+            return Content("<button>" + a + "</button>");
+        }
+    }
+}
